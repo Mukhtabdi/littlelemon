70faf284b191acba7542037793c231f4d45b214a
mukhtar <wisemuck@gmail.com>
2026-01-01 18:08:11 +0300

    start from minute 1:09:00 next time

--- restaurant/models.py
+++ restaurant/models.py
@@ -15,4 +15,7 @@ class Booking(models.Model):
 # Add code to create Menu model
 class Menu(models.Model):
    name = models.CharField(max_length=255)
-   price = models.IntegerField
+   price = models.DecimalField(max_digits=10, decimal_places=2)
+   
+   def __str__(self):
+      return self.name
